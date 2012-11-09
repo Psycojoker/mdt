47bb2c4aa2801bd4f82f588fb9fa835521c18089
Laurent Peuch <cortex@worlddomination.be>
2012-11-09 05:12:18 +0100

    [fix] bad pattern for including an url

--- project_template/project_name/urls.py
+++ project_template/project_name/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
-    # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+    # url(r'^{{ project_name }}/', include('foo.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
